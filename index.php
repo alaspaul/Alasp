81f1951c9943aa2119d4f2f76bc61b4e3e6877f1
alaspaul <53499601+alaspaul@users.noreply.github.com>
2019-07-31 09:51:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPEhtml>
+<!DOCTYPE html>
 <html>
 
 <body bgcolor="powderblue">
@@ -23,25 +23,27 @@
 <li>Graduated elementary in Pusok Elementary School</li>
 <li>Graduated Highschool in Saint Alphonsus Catholic School</li>
 <li>Graduated Senior Highschool in University of San Jose-Recoletos</li>
-</h5></i>
-</ol>
+</i></h5>
 </article>
 
 <br>
 <article>
 <h3><em>Work Experience</em></h3>
-<ul><h5>
+<ul>
+	<h5>
 <li><i>none</i></li>
-</h5>
+  </h5>
 </ul>
 </article>
 <br>
 <article>
 <h3><em>Affiliations</em></h3>
-<ul><h5><i>
+<ul>
+	<h5><i>
 <li>Dextien Xevier Alberca</li>
 <li>Reyner Paul bering</li>
-</h5></i>
+</i></h5>
+
 </ul>
 </article>
 </main>
@@ -56,10 +58,10 @@
 <aside>
 <h3><b>Parent</b></h3>
 <table style="width:100%">
-  <tr><b>
+  <tr>
     <td>Mirabel Alas</td>
     <td>Theodore alas</td> 
- </b></tr>
+ </tr>
 </aside>
 <br>
 

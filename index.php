5cf2d5ce37dbdcda99d6d6527b29c43c1962c973
alaspaul <53499601+alaspaul@users.noreply.github.com>
2019-07-31 09:39:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,18 @@
 <body bgcolor="powderblue">
 <title>Resume</title>
 </head>
-<body><center><h1> Paul Irvin E. Alas</h1><br>BSIT-1<br>
-<img src="pics.jpg" alt="picture of paul" Width="400" height="250"/></center>
-<p><u><h2><center>Welcome to my profile</u></center>
+<article>
+<header>
+<body><center><h1> Paul Irvin E. Alas</h1><br>BSIT-1<br></center>
+</header>
+</article>
+<center><img src="pics.jpg" alt="picture of paul" Width="400" height="250"/></center>
+<u><h2><center>Welcome to my profile</u></center>
 <strong><center>Hello world</center></strong>
 <h6><center><i>know me</i></h6></center>
 <br>
+<main>
+<article>
 <h3><em>Educational Background</em></h3>
 <ol><h5><i>
 <li>Graduated elementary in Pusok Elementary School</li>
@@ -16,35 +22,45 @@
 <li>Graduated Senior Highschool in University of San Jose-Recoletos</li>
 </h5></i>
 </ol>
+</article>
 
 <br>
+<article>
 <h3><em>Work Experience</em></h3>
 <ul><h5>
 <li><i>none</i></li>
 </h5>
 </ul>
+</article>
 <br>
+<article>
 <h3><em>Affiliations</em></h3>
 <ul><h5><i>
 <li>Dextien Xevier Alberca</li>
 <li>Reyner Paul bering</li>
 </h5></i>
 </ul>
+</article>
+</main>
 <h3><em>website</em></h3>
-<ul>
-<li><a href="https://twitter.com/irvin_4145"><h5>Twitter</h5></a></li>
-</ul>
 
+
+<ul>
+<footer>
+<li><a href="https://twitter.com/irvin_4145"><h5>Twitter</h5></a></li>
+</footer>
+</ul>
+<aside>
 <h3><b>Parent</b></h3>
 <table style="width:100%">
   <tr><b>
     <td>Mirabel Alas</td>
     <td>Theodore alas</td> 
  </b></tr>
+</aside>
 <br>
 
 
 
-</p>
 </body>
 </html>

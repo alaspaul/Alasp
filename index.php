4fa05f1e9a26e09d5feae4a37fcf7cb38c335dc6
alaspaul <53499601+alaspaul@users.noreply.github.com>
2019-07-31 09:24:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,50 @@
 <html>
 <head>
-  <title>hello</title>
-  </head>
-<body>
-  <h1>HELLO WORLD!!</h1>
-  </body>
+<body bgcolor="powderblue">
+<title>Resume</title>
+</head>
+<body><center><h1> Paul Irvin E. Alas</h1><br>BSIT-1<br>
+<img src="pics.jpg" alt="picture of paul" Width="400" height="250"/></center>
+<p><u><h2><center>Welcome to my profile</u></center>
+<strong><center>Hello world</center></strong>
+<h6><center><i>know me</i></h6></center>
+<br>
+<h3><em>Educational Background</em></h3>
+<ol><h5><i>
+<li>Graduated elementary in Pusok Elementary School</li>
+<li>Graduated Highschool in Saint Alphonsus Catholic School</li>
+<li>Graduated Senior Highschool in University of San Jose-Recoletos</li>
+</h5></i>
+</ol>
+
+<br>
+<h3><em>Work Experience</em></h3>
+<ul><h5>
+<li><i>none</i></li>
+</h5>
+</ul>
+<br>
+<h3><em>Affiliations</em></h3>
+<ul><h5><i>
+<li>Dextien Xevier Alberca</li>
+<li>Reyner Paul bering</li>
+</h5></i>
+</ul>
+<h3><em>website</em></h3>
+<ul>
+<li><a href="https://twitter.com/irvin_4145"><h5>Twitter</h5></a></li>
+</ul>
+
+<h3><b>Parent</b></h3>
+<table style="width:100%">
+  <tr><b>
+    <td>Mirabel Alas</td>
+    <td>Theodore alas</td> 
+ </b></tr>
+<br>
+
+
+
+</p>
+</body>
 </html>

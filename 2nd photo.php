bc58492f9957a42206dc6a949482ea487dda0966
alaspaul <53499601+alaspaul@users.noreply.github.com>
2019-08-09 23:44:28 -0700

    Update 2nd photo.php

--- 2nd photo.php
+++ 2nd photo.php
@@ -14,7 +14,7 @@
 <h4>Navigation</h4>
 	    <nav>
 		
-			 <a href="html.php">Back to Home</a><nbsb>
+			 <a href="index.php">Back to Home</a><nbsb>
 		     <a href="3rd photo.php">Next Photo</a>
 		</nav>
 
